80298c6a774c39c8f759f69ce1c20ff8344ac11a
and-gorbik <and.gorbik@gmail.com>
2021-04-23 23:32:56 +0300

    add output model for FilterAccounts

---
+++ models/output.go
@@ -0,0 +1,15 @@
+package models
+
+type AccountOut struct {
+	ID      int32   `json:"id"`
+	Email   string  `json:"email"`
+	Sex     string  `json:"sex,omitempty"`
+	Status  string  `json:"status,omitempty"`
+	Birth   int64   `json:"birth,omitempty"`
+	Fname   *string `json:"fname,omitempty"`
+	Sname   *string `json:"sname,omitempty"`
+	Phone   *string `json:"phone,omitempty"`
+	Country *string `json:"country,omitempty"`
+	City    *string `json:"city,omitempty"`
+	Premium *int64  `json:"premium,omitempty"` // ?
+}
