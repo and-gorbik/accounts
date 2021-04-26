0ef398cf2c81efd04f025802d97dc647edcf976c
and-gorbik <and.gorbik@gmail.com>
2021-04-26 21:52:04 +0300

    add business logic constraints

--- domain/constraints.go
+++ domain/constraints.go
@@ -10,8 +10,26 @@ const (
 	statusComplicated = "все сложно"
 )
 
-var (
-	maxLenInterest = 100
-	minBirth       = time.Date(1950, 1, 1, 0, 0, 0, 0, time.Local).Unix()
-	maxBirth       = time.Date(2005, 1, 1, 0, 0, 0, 0, time.Local).Unix()
+const (
+	maxLenInterest  = 100
+	maxLenEmail     = 100
+	maxLenFirstname = 50
+	maxLenSurname   = 50
+	maxLenPhone     = 16
+	maxLenSex       = 1
+	maxLenCountry   = 50
+	maxLenCity      = 50
 )
+
+var (
+	minBirth        = time.Date(1950, 1, 1, 0, 0, 0, 0, time.Local).Unix()
+	maxBirth        = time.Date(2005, 1, 1, 0, 0, 0, 0, time.Local).Unix()
+	minJoined       = time.Date(2011, 1, 1, 0, 0, 0, 0, time.Local).Unix()
+	maxJoined       = time.Date(2018, 1, 1, 0, 0, 0, 0, time.Local).Unix()
+	minPremiumStart = time.Date(2018, 1, 1, 0, 0, 0, 0, time.Local).Unix()
+	minPremiumEnd   = time.Date(2018, 1, 1, 0, 0, 0, 0, time.Local).Unix()
+)
+
+func statusIsValid(s string) bool {
+	return s == statusBusy || s == statusComplicated || s == statusFree
+}
